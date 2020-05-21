2645bd215fd8dfae294dc1b401f7d5a659515126
Kirk Swenson <kswenson@concord.org>
2020-05-20 23:45:41 -0700

    Add `serializeSelection()`; add storybook example which shows current selection dynamically

--- src/slate-editor/slate-serialization.stories.js
+++ src/slate-editor/slate-serialization.stories.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { SlateContainer } from "../slate-container/slate-container";
 import { textToSlate } from "../common/slate-types";
-import { serializeValue } from "./serialization";
+import { serializeSelection, serializeValue } from "./serialization";
 import "./slate-serialization.stories.scss";
 
 export default {
@@ -11,22 +11,43 @@ export default {
 const serializationText = "This example shows the serialized editor content.";
 
 export const Serialization = () => {
-  const [value, setValue] = useState(serializationText);
   const slateValue = textToSlate(serializationText);
-  const serializedValue = serializeValue(slateValue.toJSON());
-  const [content, setContent] = useState(serializedValue);
+  const [value, setValue] = useState(slateValue);
+  const [content, setContent] = useState(serializeValue(value.toJSON()));
   return (
     <div className="serialization-container">
       <div className="panel">
-        <SlateContainer 
-          value={value} 
-          onValueChange={_value => setValue(_value)} 
-          onContentChange={_content => setContent(_content)} 
-        />        
+        <SlateContainer
+          value={value}
+          onValueChange={_value => setValue(_value)}
+          onContentChange={_content => setContent(_content)}
+        />
       </div>
       <div className="panel output">
         <h3>Serialized Content</h3>
         <pre>{JSON.stringify(content, null, 2)}</pre>
+      </div>
+    </div>
+  );
+};
+
+const selectionSerializationText = "This example shows the serialized selection content determined by calling getDescendantsAtRange().";
+
+export const SelectionSerialization = () => {
+  const slateValue = textToSlate(selectionSerializationText);
+  const [value, setValue] = useState(slateValue);
+  const serializedSelection = serializeSelection(value);
+  return (
+    <div className="serialization-container">
+      <div className="panel">
+        <SlateContainer
+          value={value}
+          onValueChange={_value => setValue(_value)}
+        />
+      </div>
+      <div className="panel output">
+        <h3>Serialized Selection</h3>
+        <pre>{JSON.stringify(serializedSelection, null, 2)}</pre>
       </div>
     </div>
   );
