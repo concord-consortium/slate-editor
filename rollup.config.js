e11e8681899fcb79d632ed148acd510c2c7078e1
Kirk Swenson <kswenson@concord.org>
2020-07-15 21:31:52 -0700

    Configure slate-editor to use `@concord-consortium/slate-react` instead of `slate-react`

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,9 @@
+import alias from "@rollup/plugin-alias";
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
 import sass from "rollup-plugin-sass";
-
-import * as esrever from "esrever";
-import * as immutable from "immutable";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
 
 import packageJson from "./package.json";
 
@@ -26,6 +22,11 @@ export default {
     }
   ],
   plugins: [
+    alias({
+      entries: [
+        { find: 'slate-react', replacement: '@concord-consortium/slate-react' }
+      ]
+    }),
     external(),
     resolve({
       browser: true
