1deac15e7836f67f3c2df42b210cf4415aaed1c9
Scott Cytacki <scytacki@concord.org>
2022-03-24 10:18:38 -0400

    exports for a external variables plugin
    Many of them are required because parts of the dialog system are woven into into
    the built in toolbar. So these parts had to be reimplemented outside of slate-editor.
    Probably the dialog system can be refactored so it is much easier to be used by systems
    that provide their own toolbar.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 export { DocumentJSON } from "slate";
-export { Editor } from "slate-react";
+export { Editor, RenderAttributes, RenderInlineProps } from "slate-react";
 export { EditorContent, EditorRange, EditorValue, EFormat, slateToText, textToSlate } from "./common/slate-types";
 export { HtmlSerializablePlugin } from "./plugins/html-serializable-plugin";
 export { htmlToSlate, slateToHtml } from "./serialization/html-serializer";
+export { getDataFromElement, getRenderAttributesFromNode, classArray } from "./serialization/html-utils";
 export {
   deserializeValueFromLegacy, serializeValueToLegacy, validateNodeData
 } from "./serialization/legacy-serialization";
@@ -11,8 +12,10 @@ export {
 } from "./serialization/serialization";
 export { SlateContainer } from "./slate-container/slate-container";
 export { SlateEditor } from "./slate-editor/slate-editor";
-export { getContentHeight, handleToggleSuperSubscript } from "./slate-editor/slate-utils";
-export { DisplayDialogSettings, IToolOrder, OrderEntry, SlateToolbar } from "./slate-toolbar/slate-toolbar";
+export { getContentHeight, handleToggleSuperSubscript, hasActiveInline } from "./slate-editor/slate-utils";
+export { DisplayDialogSettings, IToolOrder, OrderEntry, SlateToolbar, IDialogController } from "./slate-toolbar/slate-toolbar";
+export { IFieldValues, FieldType, IRow, ModalDialog, IProps as IModalDialogProps } from "./slate-toolbar/modal-dialog";
+
 export { EditorToolbar, IButtonSpec, IToolbarColors, getPlatformTooltip } from "./editor-toolbar/editor-toolbar";
 export {
   OnChangeColorFn, OnChangeFn, OnClickFn, OnDidInvokeToolFn, OnMouseFn, ToolbarButton
